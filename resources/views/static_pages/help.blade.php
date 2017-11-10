e088a4d4141b066d3298a54811d54f11b10eae28
vagrant <vagrant@homestead>
2017-11-10 01:04:02 +0000

    完成静态页面

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,17 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>帮助页</h1>
+</body>
+</html> -->
+@extends('layouts.default')
+@section('title', '帮助')
+@section('content')
+<h1>帮助页</h1>
+@stop
