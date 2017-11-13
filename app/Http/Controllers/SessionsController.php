a15e1415769b92202c5c2f84762c414a335001e5
liuyihua <liuyihua2015@sina.com>
2017-11-13 02:15:53 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,14 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    // 只让未登录用户访问登录页面
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create(){
         return view('sessions.create');
     }
@@ -20,7 +28,8 @@ class SessionsController extends Controller
        if (Auth::attempt($credentials,$request->has('remember'))) {
            // 登录成功后的相关操作
                   session()->flash('success', '欢迎回来！');
-           return redirect()->route('users.show', [Auth::user()]);
+        //    return redirect()->route('users.show', [Auth::user()]);
+            return redirect()->intended('users.show', [Auth::user()]);
        } else {
            // 登录失败后的相关操作
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,17 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth',[
+            'except' => ['show', 'create', 'store','index']
+        ]);
+        // 只让未登录用户访问注册页面：
+         $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+
     public function create()
     {
         return view('users.create');
@@ -40,4 +51,45 @@ class UsersController extends Controller
         //用户模型 User::create() 创建成功后会返回一个用户对象，并包含新注册用户的所有信息。我们将新注册用户的所有信息赋值给变量 $user，并通过路由跳转来进行数据绑定。
         return redirect()->route('users.show', [$user]);
     }
+    public function edit(User $user){
+        $this->authorize('update', $user);
+        return view('users.edit',compact('user'));
+
+
+    }
+    public function update(User $user, Request $request){
+
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $this->authorize('update', $user);
+
+        $date = [];
+        $date['name'] = $request->name;
+        if ($request->password) {
+            $date['password'] =  bcrypt($request->password);
+        }
+        $user->update($date);
+
+        session()->flash('success','个人资料更新成功!');
+
+        return redirect()->route('users.show',$user->id);
+
+    }
+
+    public function index(){
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+    public function destroy(User $user)
+    {
+        // 删除授权策略 destroy 我们已经在上面创建了，这里我们在用户控制器中使用 authorize 方法来对删除操作进行授权验证即可。在删除动作的授权中，我们规定只有当前用户为管理员，且被删除用户不是自己时，授权才能通过。
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
+
 }
