55d5a92f66c0f41d13688a96588e08f907137482
liuyihua <liuyihua2015@sina.com>
2017-11-14 08:40:52 +0000

    Add password resets & email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -106,7 +106,7 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'liuyihua@sina.com';
+        $from = '529732278@qq.com';
         $name = 'liuyihua';
         $to = $user->email;
         $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.qq.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', '529732278@qq.com'),
+        'name' => env('MAIL_FROM_NAME', 'liuyihua'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('529732278@qq.com'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('lyh19920229'),
 
     /*
     |--------------------------------------------------------------------------
