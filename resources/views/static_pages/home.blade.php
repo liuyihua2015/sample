b739f22466bd5a689e6bf8091ff288afff9fd8f5
liuyihua <liuyihua2015@sina.com>
2017-11-16 04:11:31 +0000

    首页微博信息呈现

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,30 +1,33 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>主页</h1>
-</body>
-</html> -->
-
 @extends('layouts.default')
-@section('title', '主页')
-@section('content')
 
-<div class="jumbotron">
-    <h1>Hello Laravel</h1>
-    <p class="lead">
-      你现在所看到的是 <a href="https://fsdhub.com/books/laravel-essential-training-5.1">Laravel 入门教程</a> 的示例项目主页。
-    </p>
-    <p>
-      一切，将从这里开始。
-    </p>
-    <p>
-      <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">现在注册</a>
-    </p>
-</div>
+@section('content')
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+        <h3>微博列表</h3>
+        @include('shared._feed')
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+    <div class="jumbotron">
+      <h1>Hello Laravel</h1>
+      <p class="lead">
+        你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的项目主页。
+      </p>
+      <p>
+        一切，将从这里开始。
+      </p>
+      <p>
+        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+      </p>
+    </div>
+  @endif
 @stop
