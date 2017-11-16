61129ce7277ea36a10bcf3e5807bd17f9b540299
liuyihua <liuyihua2015@sina.com>
2017-11-16 05:59:10 +0000

    add user statuses

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -25,4 +25,15 @@ class StatusesController extends Controller
       return redirect()->back();
 
     }
+
+    public function destroy(Status $status){
+       // 这里我们使用的是『隐性路由模型绑定』功能，Laravel 会自动查找并注入对应 ID 的实例对象 $status，如果找不到就会抛出异常。
+
+      // 做删除授权的检测，不通过会抛出 403 异常。
+      $this -> authorize('destroy',$status);
+      // 调用 Eloquent 模型的 delete 方法对该微博进行删除。
+      $status->delete();
+      session()->flash('success', '微博已被成功删除！');
+      return redirect()->back();
+    }
 }
